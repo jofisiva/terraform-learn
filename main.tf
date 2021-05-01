f17b2d5581653b41b8e2ff95b56e461f14bce872
sivabalan.murugesan <sivabalan.murugesan@aimia.com>
2021-05-01 18:44:18 +0100

    Update security group and user data

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 provider "aws" {
   region = "eu-west-2"
 }
@@ -6,6 +7,10 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable public_key {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
    cidr_block = var.vpc_cidr_block
@@ -22,16 +27,16 @@ resource "aws_subnet" "myapp-subnet-1" {
      "Name" = "${var.env_prefix}-subnet"
    }
  }
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name:"${var.env_prefix}-rtb"
-  }
-}
+//resource "aws_route_table" "myapp-route-table" {
+//  vpc_id = aws_vpc.myapp-vpc.id
+//  route {
+//    cidr_block = "0.0.0.0/0"
+//    gateway_id = aws_internet_gateway.myapp-igw.id
+//  }
+//  tags = {
+//    Name:"${var.env_prefix}-rtb"
+//  }
+//}
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
   tags = {
@@ -39,7 +44,93 @@ resource "aws_internet_gateway" "myapp-igw" {
   }
 }
 
-resource "aws_route_table_association" "a-rtb-subnet" {
+//resource "aws_route_table_association" "a-rtb-subnet" {
+//  subnet_id = aws_subnet.myapp-subnet-1.id
+//  route_table_id = aws_route_table.myapp-route-table.id
+//}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+      Name:"${var.env_prefix}-main-rtb"
+    }
+}
+
+resource "aws_default_security_group" "default-sg" {
+
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+tags = {
+  Name:"${var.env_prefix}-default-sg"
+}
+}
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+output "aws_public_ip" {
+  value = aws_instance.my-app-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "terraform-file"
+  public_key = var.public_key//file(var.public_key_location)
+  //var.public_key
+}
+resource "aws_instance" "my-app-server" {
+ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
   subnet_id = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+user_data =  <<EOF
+                #!/bin/bash
+               sudo yum update -y && sudo yum install -y docker
+               sudo systemctl start docker
+               sudo usermod -aG docker ec2-user
+               docker run -p 8080:80 nginx
+           EOF
+
+  tags = {
+    "Name" = "${var.env_prefix}-server"
+  }
 }
